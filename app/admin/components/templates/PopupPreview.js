8d82c65e915e6e5f174d75fdd94a667efd8b98a5
Nikola <nikolabot@agentmail.to>
2026-05-22 00:33:09 +0000

    Revert Split Lead Magnet to previous version

--- app/admin/components/templates/PopupPreview.js
+++ app/admin/components/templates/PopupPreview.js
@@ -730,20 +730,20 @@ export default function PopupPreview({ popup, template, isMobile = false }) {
             }}>Book/Mockup</div>
           )}
           
-          {/* Shape like } character - pushed further left, extended point */}
+          {/* Shape like } character - pushed left to connect to divider */}
           <svg
             style={{
               position: 'absolute',
-              right: '10px',
+              right: '0',
               top: '50%',
               transform: 'translateY(-50%)',
-              width: '50px',
+              width: '40px',
               height: '100px'
             }}
-            viewBox="0 0 50 100"
+            viewBox="0 0 40 100"
           >
             <path
-              d="M 10,0 Q 35,25 10,50 Q 35,75 10,100 L 50,100 L 50,0 Z"
+              d="M 0,0 Q 20,25 0,50 Q 20,75 0,100 L 40,100 L 40,0 Z"
               fill="#ffffff"
             />
           </svg>
